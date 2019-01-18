2f5ff68e2f5a652fff5deb62ee9c07a1ae6e0d31
Thai Bui <tbui64@gmail.com>
2019-01-18 15:16:53 -0700

    Twilio CSS

--- server/controllers/messengerController.js
+++ server/controllers/messengerController.js
@@ -10,22 +10,13 @@ const cors = require('cors')
 
 
   module.exports = {
-      respondText: (req, res) => {
-        const twiml = new MessagingResponse();
-      
-        twiml.message('The Robots are coming! Head for the hills!');
-      
-        res.writeHead(200, {'Content-Type': 'text/xml'});
-        res.end(twiml.toString());
-      },
-
       getText: (req, res) => {
-        const {recipient, text} = req.query;
+        const { text} = req.query;
         client.messages
         .create({
            body: text,
            from: '+12093539552',
-           to: '+1' + recipient
+           to: '+12099851698'
          })
         .then(message => console.log(message.body))
         .done()
--- src/Messenger/Messenger.js
+++ src/Messenger/Messenger.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Swal from 'sweetalert2';
+import { Button } from 'react-bootstrap';
+
 
 export default class Messenger extends Component {
     constructor(props) {
@@ -16,18 +19,31 @@ export default class Messenger extends Component {
         this.setState({[prop]:value})
     }
 
-    sendText = () => {
-        const {text, recipient} = this.state;
-        axios.get(`/sms?recipient=${recipient}&text=${text}`)
+     sendText = async() => {
+
+        const {value: text} = await Swal({
+            title: 'Send a Text to your Friend!',
+            input: 'textarea',
+            inputPlaceholder: 'Motivate A Stranger To Be Accountable For Lyfe',
+            showCancelButton: true
+          })
+          
+          if (text) {
+            axios.get(`/sms?text=${text}`)
+          }
+
     }
+
+
+
 
 
     render() {
         return (
             <div>
-                <input className='recipient' onChange={(e)=>{this.handleChange('recipient', e.target.value)}} placeholder='recipient'/>
-                <input className='text' onChange={(e)=>{this.handleChange('text', e.target.value)}} placeholder='text'/>
-                <button onClick={()=>{this.sendText()}}> Text Message! </button>
+                {/* <input className='recipient' onChange={(e)=>{this.handleChange('recipient', e.target.value)}} placeholder='recipient'/>
+                <input className='text' onChange={(e)=>{this.handleChange('text', e.target.value)}} placeholder='text'/> */}
+                <Button onClick={()=>{this.sendText()}}> Text Message! </Button>
             </div>
         )
     }
